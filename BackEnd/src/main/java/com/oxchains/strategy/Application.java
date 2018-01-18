14fd9cbea59fc61307e13f1b9678b221ae7d2270
luoxuri <txwwze@126.com>
2018-01-18 16:54:15 +0800

    create spring boot project

---
+++ BackEnd/src/main/java/com/oxchains/strategy/Application.java
@@ -0,0 +1,15 @@
+package com.oxchains.strategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luoxuri
+ * @create 2018-01-18 16:46
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
